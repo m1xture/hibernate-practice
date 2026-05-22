4550bf60a89405869f006a9de4768dfab7562d54
m1xture <m1xture.dev@gmail.com>
2026-05-22 15:50:06 +0300

    hw40 Spring Boot

--- maven/hw37/src/main/java/com/java/app/hw37/controller/PingController.java
+++ maven/hw37/src/main/java/com/java/app/hw37/controller/PingController.java
@@ -1,15 +1,19 @@
-package com.java.app.hw40.controller;
+package com.java.app.hw37.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("ping")
+@RequestMapping("/ping")
+@RequiredArgsConstructor
 public class PingController {
 
-    @GetMapping
+    @GetMapping()
     public String ping() {
         return "OK";
     }
+
 }
